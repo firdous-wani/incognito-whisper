6b613bc3cd0f357c5488d2c175afd9b605d03e1d
firdous-wani <azharwani7081@gmail.com>
2024-07-19 00:25:22 +0530

    feat: Added email domain for sending verification mails

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,7 +9,7 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "noreply@incognito-whisper.in",
       to: email,
       subject: "Incognito Whisper Verification Code",
       react: VerificationEmail({ username, otp: verifyCode }),
